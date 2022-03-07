a5022a4d8440dad4db14b5fac4a661041e7ebd4f
fabiohigoralm <fabiohigoralm2@gmail.com>
2022-03-06 23:25:32 -0300

    remove imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Container, Header } from "./App.styles";
 import { ProductList } from "./components/ProductList";
-import icon_b8one from "./icon/b8one_icon.svg"
 import icon_type from "./icon/typescript_react_100px.png"
 
 const App = () =>{
-  const img_b8one = <img src={icon_b8one} alt="logo b8one" />;
   const img_type = <img src={icon_type} alt="logo react + typeScript" />;
   return(
     <>
-    <Header>{img_b8one}{img_type}</Header>
+    <Header>{img_type}</Header>
      <Container>
        <ProductList />
      </Container>
